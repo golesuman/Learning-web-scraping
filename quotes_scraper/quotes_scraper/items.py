1a3478b3c183b3e3f7cadc22b7f7105c62f14feb
Suman Gole <suman.gole@truenary.com>
2022-10-31 13:21:40 +0545

    add: upload scraped data to the database

--- quotes_scraper/quotes_scraper/items.py
+++ quotes_scraper/quotes_scraper/items.py
@@ -9,6 +9,6 @@ import scrapy
 class QuotesScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    quotes = scrapy.Field()
+    quote = scrapy.Field()
     author = scrapy.Field()
     
--- quotes_scraper/quotes_scraper/pipelines.py
+++ quotes_scraper/quotes_scraper/pipelines.py
@@ -5,9 +5,36 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
 from itemadapter import ItemAdapter
 
 
 class QuotesScraperPipeline:
+    def __init__(self) -> None:
+        self.create_connection()
+        self.create_table()
+        
+         
+    def create_connection(self):
+        self.conn = sqlite3.connect("myquotes.db")
+        self.curr = self.conn.cursor()
+    
+    def create_table(self):
+        self.curr.execute('''drop table if exists quotes_tb''')
+
+        self.curr.execute('''create table quotes_tb (
+                             quote text, 
+                             author text
+                         )''')
+
+    def store_db(self, item):
+        query = """insert into quotes_tb ('quote', 'author') values(?, ?)"""
+        data = (item['quote'],item["author"])
+        self.curr.execute(query, data)
+        self.conn.commit()
+
+
     def process_item(self, item, spider):
+        self.store_db(item)
+        # print(f"Pipeline :-  {item['quotes'][0]}")
         return item
